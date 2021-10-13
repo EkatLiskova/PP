99e13f9df413ec9318901cebf16321319c306f08
Дмитрий <seentez@hotmail.com>
2021-10-14 02:27:50 +0500

    lab1

--- PP/Source.cpp
+++ PP/Source.cpp
@@ -1,32 +1,21 @@
 #include <iostream>
 #include "mpi.h"
+using namespace std;
 
 int main(int argc, char* argv[]) {
 	MPI_Init(&argc, &argv);
 
-	int world_rank, count_ranks;
-	MPI_Comm_size(MPI_COMM_WORLD, &count_ranks);
+	int world_rank;
 	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
 	if (world_rank == 0) {
-		int send_number = 1, recv_number = 0;
+		int send_number = 111;
 		MPI_Send(&send_number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
-		MPI_Recv(&recv_number, 1, MPI_INT, (count_ranks - 1), 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-		send_number = recv_number + 1;
-		MPI_Send(&send_number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
-		MPI_Recv(&recv_number, 1, MPI_INT, (count_ranks - 1), 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-		printf("%d %d", world_rank, recv_number);
 	}
-	else {
-		int recv_number = 0, send_number = 0;
-		int next = world_rank == count_ranks - 1 ? 0 : (world_rank + 1);
-		MPI_Recv(&recv_number, 1, MPI_INT, (world_rank - 1), 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-		send_number = recv_number + 1;
-		MPI_Send(&send_number, 1, MPI_INT, next, 0, MPI_COMM_WORLD);
-		MPI_Recv(&recv_number, 1, MPI_INT, (world_rank - 1), 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-		send_number = recv_number + 1;
-		MPI_Send(&send_number, 1, MPI_INT, next, 0, MPI_COMM_WORLD);
-		printf("%d %d", world_rank, recv_number);
+	else if (world_rank == 1) {
+		int recv_number = NULL;
+		MPI_Recv(&recv_number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		cout << "Hello to process 1 from process 0! Recieved number: " << recv_number << endl;
 	}
 
 	MPI_Finalize();
